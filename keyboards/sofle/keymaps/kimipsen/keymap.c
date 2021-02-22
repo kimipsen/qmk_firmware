022175872e27a1541eccbe0fa991b9df4de62cbf
Kim Ipsen <kip@mindworking.dk>
2021-02-22 19:10:42 +0100

    tapdance

--- keyboards/sofle/keymaps/kimipsen/keymap.c
+++ keyboards/sofle/keymaps/kimipsen/keymap.c
@@ -36,8 +36,7 @@ enum tap_dance_keycodes {
     TD_7_F7,
     TD_8_F8,
     TD_9_F9,
-    TD_0_F10,
-    TD_SPC_ENT
+    TD_0_F10
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
@@ -50,8 +49,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_7_F7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_F7),
   [TD_8_F8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_F8),
   [TD_9_F9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_F9),
-  [TD_0_F10] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_F10),
-  [TD_SPC_ENT] = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_ENT)
+  [TD_0_F10] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_F10)
 };
 
 
@@ -76,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   TD(TD_1_F11),   TD(TD_2_F2),    TD(TD_3_F3),    TD(TD_4_F4),    TD(TD_5_F5),                      TD(TD_6_F6),    TD(TD_7_F7),    TD(TD_8_F8),    TD(TD_9_F9),    TD(TD_0_F10),  KC_GRV, \
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LGUI,KC_LALT,KC_LCTRL, KC_LOWER, TD(TD_SPC_ENT),      TD(TD_SPC_ENT),  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     LALT(KC_PSCREEN), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
+                 KC_LGUI,KC_LALT,KC_LCTRL, KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI \
 ),
 /*
  * COLEMAK
@@ -100,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC, \
   KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LGUI,KC_LALT,KC_LCTRL,KC_LOWER, TD(TD_SPC_ENT),      TD(TD_SPC_ENT),  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI \
+                 KC_LGUI,KC_LALT,KC_LCTRL,KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
